9d939dd27c0445de283f6b945fbf32fa72acaec3
Grant <grant.hoffman.phone@gmail.com>
2016-11-19 08:48:53 -0800

    Finially working

--- server/serverModules/routes/Root.js
+++ server/serverModules/routes/Root.js
@@ -14,8 +14,7 @@ var path = require('path');
 
 function root(app, config) {
     app.get('/', function (req, res) {
-        // res.sendFile(path.resolve(__dirname, '../../../application/build/index.html'));
-        res.send('This is a start all the way!');
+        res.sendFile(path.resolve(__dirname, '../../../application/build/index.html'));
     });
 }
 
