6dacc6aaf403c73cf11107c3f0201948647cf2fd
Grant <grant.hoffman.phone@gmail.com>
2016-11-22 08:25:24 -0800

    Trouble shooting redirect

--- server/serverModules/routes/Misc.js
+++ server/serverModules/routes/Misc.js
@@ -3,16 +3,15 @@
  */
 var path = require('path'),
     proccess = require('../process/ProcessUrls'),
-    baseUrl = 'http://glhbit.tk';
+    baseUrl = 'http://glhbit.tx';
 
 function root(app, config) {
     app.get('/*', function (req, res, next) {
         if (req.originalUrl !== "/" && req.originalUrl !== '/__webpack_hmr' && req.originalUrl !=='/favicon.ico' && req.originalUrl !=='/create') {
             proccess(baseUrl + req.originalUrl, function (error, results) {
                 if (!error && results) {
-                    // res.statusCode(301);
-                    // res.redirect(results.Url);
-                    res.send(results.Url);
+                    res.statusCode(301);
+                    res.redirect(results.Url);
                 } else {
                     res.statusCode(404).end();
                 }
