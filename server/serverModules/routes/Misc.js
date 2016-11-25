e590d257e77c7e9d9d637b6b2740571e1722d569
Grant <grant.hoffman.phone@gmail.com>
2016-11-25 10:45:34 -0800

    Adding some logging to solve redirect

--- server/serverModules/routes/Misc.js
+++ server/serverModules/routes/Misc.js
@@ -8,8 +8,10 @@ var path = require('path'),
 function root(app, config) {
     app.get('/*', function (req, res, next) {
         if (req.originalUrl !== "/" && req.originalUrl !== '/__webpack_hmr' && req.originalUrl !=='/favicon.ico' && req.originalUrl !=='/create') {
+            console.log('Incoming URL: ' + baseUrl + req.originalUrl);
             proccess(baseUrl + req.originalUrl, function (error, results) {
                 if (!error && results) {
+                    console.log('Outging URL: ' + baseUrl + req.originalUrl);
                     res.statusCode(301);
                     res.redirect(results.Url);
                 } else {
@@ -17,6 +19,7 @@ function root(app, config) {
                 }
             });
         } else {
+            console.log('We skipped URL Processing!');
             next();
         }
     });
